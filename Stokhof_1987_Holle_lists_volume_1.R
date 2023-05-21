3d8f81ed462523b7664daca993bade992fbc6517
Gede Rajeg <primahadiwijaya@gmail.com>
2023-05-21 15:24:14 +0800

    Add the R code to join the Holle List and the Enggano List.

---
+++ Stokhof_1987_Holle_lists_volume_1.R
@@ -0,0 +1,14 @@
+# code to process the digitised Holle's list in Stokhof and Almanar (1987)
+# load the package =====
+library(tidyverse)
+library(readxl)
+
+holle_tb <- read_xlsx("Stokhof_1987_Holle_lists_volume_1.xlsx", sheet = 1, range = "A1:G1632")
+holle_phrases_1931 <- read_xlsx("Stokhof_1987_Holle_lists_volume_1.xlsx", sheet = 2)
+holle_phrases_1940_11 <- read_xlsx("Stokhof_1987_Holle_lists_volume_1.xlsx", sheet = 3)
+
+# merging the Enggano list and the Holle list =====
+source("Stokhof_1987_Holle_lists_Enggano.R")
+eno_form_df_trim |> 
+  left_join(holle_tb |> rename(id = Index),
+            by = "id")
